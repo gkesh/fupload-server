0fc0df3792f0582a206f48ad762b5db88fb1a623
gkesh <gkeshthapa@gmail.com>
2023-03-28 12:34:40 +0545

    Fixing random cors issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,6 +10,16 @@ export default (app: Application): Application => {
     })
   );
 
+  app.use((_, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
   app.use(connectBusboy());
 
   return app;
